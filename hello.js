8879404e8cfc6130113c10b38d373759511e5b1f
Diksha048 <bhagwatdiksha0408@gmail.com>
2024-07-10 20:44:36 +0530

    hello to array elemnt code added

---
+++ hello.js
@@ -0,0 +1,6 @@
+const name = ["Harshad", "Falguni", "Diksha", "Pranjali", "Vedant", "Prachi", "Mahi", "Shreya", "Nancy"];
+
+for(let key in name)
+    {
+        console.log(`Hello ${name[key]}!`)
+    }
